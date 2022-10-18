6e919c93a2750f35af876d3cfc684a785039582e
AAYKK <116094602+AAYKK@users.noreply.github.com>
2022-10-18 18:39:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='word2text_ru',
+setup(name='num2text',
       version='0.1',
       url='https://github.com/AAYKK/word2text_ru',
       author='Ayk Akopyan',
       author_email='ayk.work@mail.ru',
       description='Перевод чисел в текст по-русски',
-      packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
       zip_safe=False)
